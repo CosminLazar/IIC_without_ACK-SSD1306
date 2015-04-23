8a2b5565b98efe0efb14fc127245a86095deb118
Cosmin Lazar <ccl@d60.dk>
2015-04-23 21:41:17 +0200

    resharper reformat

--- IIC_without_ACK.h
+++ IIC_without_ACK.h
@@ -6,7 +6,7 @@
 * 后续会完善功能的！！！！
 *
 * 最新版本的“IIC_wtihout_ACK”会发布在 http://www.14blog.com/archives/1358
-* 
+*
 * 建议和问题反馈，请发邮件至 hello14blog@gmail.com
 ***************************************************************************/
 
@@ -14,9 +14,9 @@
 #define _IIC_without_ACK_H
 
 #if ARDUINO >= 100
- #include "Arduino.h"
+#include "Arduino.h"
 #else
- #include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 //#include <Adafruit_GFX.h>
@@ -26,23 +26,23 @@
 
 class IIC_without_ACK
 {
- public:
-  IIC_without_ACK(int sda, int scl);
-  
-  void IIC_Start();
-  void IIC_Stop();
-  void Write_IIC_Byte(unsigned char IIC_Byte);
-  void Write_IIC_Command(unsigned char IIC_Command);
-  void Begin_IIC_Data();
-  void IIC_SetPos(unsigned char x, unsigned char y);
-  void Fill_Screen(unsigned char fill_Data);
-  void Char_F6x8(unsigned char x, unsigned char y, const char ch[]);
-//  void Char_F8x16(unsigned char x, unsigned char y,const char ch[]);
-//  void CN_F16x16(unsigned char x, unsigned char y, unsigned char N);
-  void Draw_BMP(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1,const char BMP[]);
-  void Initial();
-  
-  private:
-  int _sda, _scl;
+public:
+	IIC_without_ACK(int sda, int scl);
+
+	void IIC_Start();
+	void IIC_Stop();
+	void Write_IIC_Byte(unsigned char IIC_Byte);
+	void Write_IIC_Command(unsigned char IIC_Command);
+	void Begin_IIC_Data();
+	void IIC_SetPos(unsigned char x, unsigned char y);
+	void Fill_Screen(unsigned char fill_Data);
+	void Char_F6x8(unsigned char x, unsigned char y, const char ch[]);
+	//  void Char_F8x16(unsigned char x, unsigned char y,const char ch[]);
+	//  void CN_F16x16(unsigned char x, unsigned char y, unsigned char N);
+	void Draw_BMP(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, const char BMP[]);
+	void Initial();
+
+private:
+	int _sda, _scl;
 };
 #endif
